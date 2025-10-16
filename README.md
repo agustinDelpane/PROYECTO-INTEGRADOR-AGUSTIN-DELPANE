4b63ce0b87c57c18bfa3e0209d30ab6f44ad9014
Agustin Delpane <agustindelpane@gmail.com>
2025-10-16 13:09:48 -0300

    Commit con backend y frontend

--- README.md
+++
@@ -1,5 +0,0 @@
-# PROYECTO-INTEGRADOR-AGUSTIN-DELPANE
-
-Link Vercel: https://proyecto-integrador-agustin-delpane.vercel.app
-
-**PS**: Me gustaria haber retocado algunos elementos para que se adapten al diseño responsive, pero no pude por falta de tiempo.
--- src/api/data/values.js
+++
@@ -1,8 +0,0 @@
-export const values = `Nuestros valores están siempre con el cliente y a favor de facilitar el mundo de la música hacia
-                    cualquier persona que esté interesada. Nos respaldamos en una gran atención y servicio de calidad
-                    para servir al cliente de la mejor forma. ¡Cualquier interés o pregunta es válida! No es necesario
-                    tener conocimientos de música para ser cliente de esta gran tienda de música. Nadie va a juzgarte
-                    por no saber cómo suena un Si bemol en afinación Drop C#. Aunque no sepas ni cómo cambiar una
-                    cuerda, ¡no hay problema! Nuestros profesionales van a estar a disposición para ayudarte y
-                    despejarte cualquier duda que tengas para que puedas decidir si te gustaría entrar en el maravilloso
-                    mundo de la música con el instrumento que más te guste.`;
