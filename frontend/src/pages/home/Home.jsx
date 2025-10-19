1b7ddf3dfd7a972975d31284868c5e10fd2ad701
Agustin Delpane <agustindelpane@gmail.com>
2025-10-19 11:30:37 -0300

    Agrego estilos

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import { Text } from "@/components/texts";
 import "./home.scss";
+import AdSlider from "./ad-slider/AdSlider";
+import HighlightedProducts from "./highlighted-products/HighlightedProducts";
 
 const Home = () => {
     return (
         <div className="home">
             <Text variant="h2">Inicio</Text>
+            <AdSlider />
+            <HighlightedProducts/>
         </div>
     );
 };
